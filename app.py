d212cb9b561e1a2a7b946e77bce9dab07403ab54
Adisa Barakat <adisabarakatadekemi@gmail.com>
2023-03-19 18:43:26 +0100

    Second fix on config

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from api.config.config import config_dict
 
 app = create_app(config=config_dict['prod'])
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
